32e1f19ac1ba4ae0c808ef4c0de92a5e3bd0f0a7
roson9527 <roson9527@sina.cn>
2019-11-26 11:57:15 +0800

    style(format): test

--- path/base/convert_test.go
+++ path/base/convert_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestFormatData(t *testing.T) {
-	data:="0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675"
-	ret, _ :=FormatData(data, "hex")
+	data := "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675"
+	ret, _ := FormatData(data, "hex")
 	fmt.Println(ret)
 }
