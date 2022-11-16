d1743ad1737d2eb109d59788cf9a621570338837
roson9527 <roson9527@sina.cn>
2022-11-16 16:58:20 +0800

    feat(client): more friendly signature interaction

---
+++ client/wallet.go
@@ -0,0 +1,65 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+func (c *Client) WalletList(project string) ([]string, error) {
+	sec, err := c.Meta.Logical().List(c.conf.SecretPath + fmt.Sprintf(PatternWallet, project, ""))
+	if err != nil {
+		return nil, err
+	}
+
+	// 没有数据
+	if sec == nil {
+		return []string{}, nil
+	}
+
+	out := make([]string, 0)
+	for _, v := range sec.Data["keys"].([]any) {
+		out = append(out, fmt.Sprintf("%s", v))
+	}
+
+	return out, nil
+}
+
+func (c *Client) ReadWallet(project, address string) (*Wallet, error) {
+	sec, err := c.Meta.Logical().Read(c.conf.SecretPath + fmt.Sprintf(PatternWallet, project, address))
+	if err != nil {
+		return nil, err
+	}
+
+	// 没有数据
+	if sec == nil {
+		return nil, nil
+	}
+
+	out := &Wallet{
+		Address:   sec.Data["address"].(string),
+		PublicKey: sec.Data["public_key"].(string),
+	}
+	out.UpdateTime, _ = sec.Data["update_time"].(json.Number).Int64()
+	return out, nil
+}
+
+func (c *Client) SignTx(project, address string, unsignTx *types.Transaction) (*types.Transaction, error) {
+	payload := make(map[string]any)
+	data, err := unsignTx.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+	payload[fieldTxBinary] = hexutil.Encode(data)
+
+	sec, err := c.Meta.Logical().Write(c.conf.SecretPath+fmt.Sprintf(PatternWallet, project, address)+"/sign_tx", payload)
+	if err != nil {
+		return nil, err
+	}
+
+	var signedTx types.Transaction
+	_ = signedTx.UnmarshalBinary(hexutil.MustDecode(sec.Data["tx_binary"].(string)))
+
+	return &signedTx, nil
+}
